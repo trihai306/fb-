80948a062a602dda3980281019e9bf62ee7e90ab
khoa <demonhunter942000@gmail.com>
2023-04-15 19:57:22 +0700

    update, fix f5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useEffect } from 'react'
 
 // ** Router Import
 import Router from './router/Router'
 
 const App = () => {
   // window["eel"].set_host("ws://localhost:8080");
+  useEffect(() => {
+    const handleRefresh = (event) => {
+      if (event.keyCode === 116 || (event.ctrlKey && event.keyCode === 82)) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    };
+
+    window.addEventListener('keydown', handleRefresh);
+
+    return () => {
+      window.removeEventListener('keydown', handleRefresh);
+    };
+  }, []);
   return (
     <Suspense fallback={null}>
       <Router />
