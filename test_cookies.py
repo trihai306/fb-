14d8b236c3401a847198a5033272ae5a4020b078
khoa <demonhunter942000@gmail.com>
2023-04-23 11:36:32 +0700

    update cookies

--- test_cookies.py
+++ test_cookies.py
@@ -5,25 +5,34 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-# 1 . khai báo biến browser
-options = Options()
-# options.add_argument("--headless")  # prevent show mock browser
+import random
+import sys
 
-service = Service(executable_path="./chromedriver")
-browser = webdriver.Chrome(service=service, options=options)
+def login(username, password):
+    # 1 . khai báo biến browser
+    options = Options()
+    # options.add_argument("--headless")  # prevent show mock browser
 
-# 1. Open Facebook
-browser.get("http://facebook.com")
+    service = Service(executable_path="./chromedriver")
+    browser = webdriver.Chrome(service=service, options=options)
 
-# 2. Try to login
-email_form = browser.find_element(By.ID, 'email')
-email_form.send_keys("") # emails
+    # 1. Open Facebook
+    browser.get("http://mbasic.facebook.com")
 
-password_form = browser.find_element(By.ID, "pass")
-password_form.send_keys("") # password
-password_form.send_keys(Keys.ENTER)
-sleep(10)
+    # 2. Try to login
+    email_form = browser.find_element(By.ID, 'm_login_email')
+    email_form.send_keys(username) # emails
 
-pickle.dump(browser.get_cookies(), open("my_cookies.pkl", "wb"))
+    password_form = browser.find_element(By.ID, "password_input_with_placeholder").find_element(By.CSS_SELECTOR, 'input')
+    password_form.send_keys(password) # password
+    password_form.send_keys(Keys.ENTER)
+    sleep(random.randint(3,5))
 
-# browser.close()
+    ok_btn = browser.find_element(By.CSS_SELECTOR, 'form')
+    ok_btn.click()
+
+    sleep(random.randint(3,5))
+
+    pickle.dump(browser.get_cookies(), open("my_cookies.pkl", "wb"))
+
+login(sys.argv[1], sys.argv[2])
